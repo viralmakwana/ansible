532fd9784287ddf6f636ab49ea840ee5f62b1b94
viral <root@ansible.server1.local>
2015-04-15 11:04:33 +0100

    logic

--- changepw-interactive.sh
+++ changepw-interactive.sh
@@ -5,32 +5,28 @@
 HOST=$(hostname)
 
 echo "Please tell me your user name, then press [ENTER]: "
-read -rse user
+read -re user
 
-<<<<<<< HEAD
 if $(grep "$user" /etc/passwd > /dev/null 2>&1); then 
 	echo "$user does exist, exiting"
 exit 1
-=======
-if [ id -u "$user" > /dev/null 2>&1 ] ; then echo "$user does exist, lets continue....";
->>>>>>> c3d0f7485882de1d366eb4c9785c26108e591455
 	else echo "$user does not exist on $HOST, would you like to add this user [y/n]: "
 	read -rse adduser
 if [ "$adduser" == "y" ]; then
 	useradd -m $user
 elif [ "$adduser" == "n" ]; then
 	echo "Ok I will not create $user"
-exit 500	
+exit 2	
 fi
 
 echo  "Adding password for $user, Please type in your password, then press [ENTER]: "
 
-read -r password
+read -rs password
 if [ -n $password ]; then
 	echo "$password" | passwd --stdin $user
 else
 	echo "no password supplied, please double check"
-	exit 501
+	exit 3
 fi
 fi
 
